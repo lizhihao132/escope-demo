48ed837d4202ed5c92b6c0a4b099f2e51b06e83e
edgarlli <edgarlli@tencent.com>
2023-05-19 01:23:58 +0800

    优化 webpack 和各个功能库为最新版本

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,78 @@
 var path = require('path');
 var webpack = require('webpack');
-
-
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
     entry: './src/scripts/app.js',
+    mode: "production", // 似乎不加这一行, 页面上的 ast 节点名字被 mangle 了... 且 npm run build 也会有 warnning.
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'scripts/bundle.js'
     },
     devtool: 'inline-source-map',
     module: {
-        loaders: [
-            { test: /\.json$/, loader: "json" },
-            { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },
-            { test: /\.css$/, loader: ExtractTextPlugin.extract("css-loader") },
+        rules: [
+            // { test: /\.json$/, loader: "json-loader" },	//Since webpack >= v2.0.0, importing of JSON files will work by default
+            {
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 100000
+                }
+            },
             {
                 test: path.join(__dirname, 'src/scripts'),
                 loader: 'babel-loader'
+            },
+
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
             }
         ]
     },
     plugins: [
-        new ExtractTextPlugin("styles/[name].css", { allChunks: true }),
         // new webpack.optimize.UglifyJsPlugin({minimize: false})
-        new CopyWebpackPlugin([
-            {
-                'from': 'app/',
-                'to': '.'
-            }
-        ])
-    ]
+
+        new CopyWebpackPlugin({
+            patterns: [{
+                from: "app/",
+                to: "."
+            }],
+        }),
+
+        new MiniCssExtractPlugin({
+            filename: 'styles/[name].css',
+            chunkFilename: 'styles/[id].css',
+            //allChunks: true
+        }),
+
+        // fix "process is not defined" error:
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        }),
+    ],
+
+	performance: {
+		hints: false,	// 不再提示性能问题
+		maxEntrypointSize: 512000,
+		maxAssetSize: 512000
+	},
+    optimization: {
+        minimize: false,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    compress: false,
+                    mangle: false,
+                    output: {
+                        beautify: true,
+                        comments: true,
+                    },
+                },
+            }),
+        ],
+    },
 };
