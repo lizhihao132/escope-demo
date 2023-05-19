ab7851730ea3e679553bbd4db2d9f9233d987d84
edgarlli <edgarlli@tencent.com>
2023-05-19 15:09:41 +0800

    11

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,7 +2,9 @@ require('codemirror/lib/codemirror.css');
 require('codemirror/theme/monokai.css');
 require('../styles/main.css');
 require('bootstrap/dist/css/bootstrap.css');
+
 require('@fortawesome/fontawesome-free/css/all.css');
+//import '@fortawesome/fontawesome-free/css/all.css';
 
 
 var CodeMirror = require('codemirror');
@@ -10,7 +12,8 @@ require('codemirror/mode/javascript/javascript');
 var $ = require('jquery');
 var escope = require('escope');
 var esprima = require('esprima');
-
+let acorn = require('acorn')
+let eslint_scope = require('eslint-scope');
 
 var run = function() {
     'use strict';
@@ -138,16 +141,35 @@ var run = function() {
         }
         return result;
     };
+	
+	let updateLibInfo = function(parserLib, scopeLib){
+		$('#use_which_scope').text(parserLib);
+		$('#use_which_parser').text(scopeLib);
+			
+		if('esprima' == parserLib){
+			$('#parser-version').text(esprima.version);
+		}
+		else if('acorn' == parserLib){
+			$('#parser-version').text(acorn.version);
+		}
+		
+		if('escope' == scopeLib){
+			$('#scope-version').text(escope.version);
+		}else if('eslint-scope' == scopeLib){
+			$('#scope-version').text(eslint_scope.version);
+		}
+	}
 
 	let getAst = function(code, use_acron_if_esprima_failed, ecmaVersion, sourceType, extra_info){
 		var ast = null;
+		let parserLib, scopeLib;
 		try{
+			extra_info.use_parser = 'esprima'
 			ast = esprima.parse(code, {
 				range: true,
 				loc: true,
 				sourceType: sourceType,
 			});
-			console.info('parse ast by esprima success')
 		}catch(err){
 			if(use_acron_if_esprima_failed)
 			{	
@@ -160,18 +182,21 @@ var run = function() {
 		}
 		
 		if(!ast && !use_acron_if_esprima_failed){
+			console.info('---- failed, and not use acorn')
 			return null
 		}
 		
 		let acornOption = {ranges: true, locations: true, ecmaVersion: ecmaVersion, sourceType: sourceType, allowReserved: true, };
-		let acorn = require('acorn')
+		
 		try{
+			extra_info.use_parser = 'acorn'
 			ast = acorn.parse(code, acornOption);
 			console.info('parse ast by acron 1 success')
 		}
 		catch(err){
 			extra_info.exception = err
 			try{
+				extra_info.use_parser = 'acorn'
 				ast = acorn.parse(code, {ranges: true, locations: true})	//对于这样的代码在高版本的 js 中是有效的: a = {name: 1}; b={hei:3}; c={...a,...b};
 				console.info('parse ast by acron 2 success')
 			}catch(err){
@@ -187,13 +212,10 @@ var run = function() {
 	}
 	
 	let getScopeList = function(ast, scopeLib, ecmaVersion, sourceType){
-		
-		
 		var scopes = null;
 		if('escope' == scopeLib){
 			scopes = escope.analyze(ast, {sourceType: sourceType, ecmaVersion: parseInt(ecmaVersion)}).scopes;
-		}else if('eslint_scope' == scopeLib){
-			let eslint_scope = require('eslint-scope');
+		}else if('eslint-scope' == scopeLib){
 			scopes = eslint_scope.analyze(ast, {sourceType: sourceType, ecmaVersion: parseInt(ecmaVersion)}).scopes;
 		}
 		
@@ -247,15 +269,19 @@ var run = function() {
 			}
 		}
 		
+		//console.info('------------------- lineNumber:', lineNumber, ', column:', column, ', index:', index)
+		
 		let npos = parseInt(index)
 		
-		return `"${exception.description}" at line-column-index: (${lineNumber}, ${column}, ${index}) <br><br> ${code.substring(npos-20, npos)}<b><font color='red' size='14px'>${code.charAt(npos)}</font></b>${code.substring(npos+1, 20)}`
+		let ret = `"${exception.message}" at line-column-index: (${lineNumber}, ${column}, ${index}) <br><br> ${code.substring(npos-20, npos)}<b><font color='red' size='14px'>${code.charAt(npos)}</font></b>${code.substring(npos+1, npos+20)}`
+		
+		return ret
 	}
 	
 	let ecmaVersion,sourceType
     var body = $("body")
     var draw = function() {
-        body.removeClass("bg-danger");
+        body.removeClass("bg-warning");
 		
 		let ecmaVersion = $('input[name="es"]:checked').val();
 		let sourceType = $('input[name="st"]:checked').val();
@@ -263,28 +289,35 @@ var run = function() {
 		let use_acron_if_esprima_failed = $('input[name="use_acron_if_esprima_failed"]:checked').val() == 'yes' 
 		
 		console.info('parse params, ecmaVersion:', ecmaVersion, ', sourceType:', sourceType, ', scopeLib:', scopeLib, ', use_acron_if_esprima_failed:', use_acron_if_esprima_failed)
+		let extra_info = {}
 		
+		//默认 parser 是 esprima, 当 parser 失败时, 使用 acorn
+		updateLibInfo('esprima', scopeLib)
         try{
 			$('#treeview').html('');
 			
 			let code = editor.getValue()
 			if(code.length == 0) return;
-			let extra_info = {}
+			
 			let ast = getAst(code, use_acron_if_esprima_failed, ecmaVersion, sourceType, extra_info)
 			if(!ast){
-				body.addClass("bg-danger");
+				body.addClass("bg-warning");
 				console.info(extra_info.exception)
 				$('#treeview').html(formatErrorToHtml(extra_info.exception, code));
-				return
+			}else{
+				let scopes = getScopeList(ast, scopeLib, ecmaVersion, sourceType, extra_info)
+				let nodes = traverseNode(scopes, true);
+				$('#treeview').append(nodes);
 			}
-			var scopes = getScopeList(ast, scopeLib, ecmaVersion, sourceType)
-			var nodes = traverseNode(scopes, true);
-			$('#treeview').append(nodes);
         } catch(e){
-            body.addClass("bg-danger");
+            body.addClass("bg-warning");
             console.error(e);
 			$('#treeview').html(formatErrorToHtml(e, code));
         }
+		
+		// 当解析完成时, 应当报告最后一次使用的 parser
+		console.info('last failed parser:', extra_info.use_parser)
+		updateLibInfo(extra_info.use_parser, scopeLib)
     };
 
     var editor = CodeMirror.fromTextArea($('#editor')[0], {
@@ -302,9 +335,7 @@ var run = function() {
 
     draw();
 
-    $('#escope-version').text(escope.version);
-    $('#esprima-version').text(esprima.version);
-
+	
 };  // end run
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,87 +1,76 @@
-var path = require('path');
-var webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
-const TerserPlugin = require("terser-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/scripts/app.js',
-    mode: "production", // 似乎不加这一行, 页面上的 ast 节点名字被 mangle 了... 且 npm run build 也会有 warnning.
+    mode: "production",
     output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'scripts/bundle.js'
+        path: __dirname + '/dist',
+        filename: 'scripts/bundle.js',
     },
-    devtool: 'inline-source-map',
     module: {
         rules: [
-            // { test: /\.json$/, loader: "json-loader" },	//Since webpack >= v2.0.0, importing of JSON files will work by default
-            {
+			/*
+			下面这个配置会导致页面上的 font-awesome 显示不正常, 原因暂未知
+			{
                 test: /\.(png|woff|woff2|eot|ttf|svg)$/,
                 loader: 'url-loader',
                 options: {
                     limit: 100000
                 }
-            },
-			/* github Action 会报错
+            },	
+			*/
+			/*
             {
-                test: path.join(__dirname, 'src/scripts'),
-                loader: 'babel-loader'
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
             },
 			*/
 
             {
                 test: /\.css$/,
+                // use: ['style-loader', 'css-loader']	//style-loader 是动态插入 <style> 对调试友好; MiniCssExtractPlugin 是生成整个 css 文件,性能更好
                 use: [MiniCssExtractPlugin.loader, 'css-loader']
-				/*
-				use: ['style-loader', {
-					loader: "css-loader",
-					options: {
-					  import: true,
-					  url: {
-						  // 将所有的 URL 路径都视为绝对路径，不进行解析
-						  filter: (url) => !url.startsWith('/'),
-						},
-
-					},
-				}]
-				*/
-				
-            },
-			 {
-			   test: /.(woff|woff2|eot|ttf|otf)$/,
-			   use: [
-				 'file-loader',
-			   ]
-			 }
-        ]
+            }
+        ],
     },
     plugins: [
-        // new webpack.optimize.UglifyJsPlugin({minimize: false})
+        new HtmlWebpackPlugin({
+            template: './static/index.html',
+            minify: false, // 禁用压缩功能
+            inject: false, // 禁止字符串变换
 
-        new CopyWebpackPlugin({
-            patterns: [{
-                from: "app/",
-                to: "."
-            }],
         }),
-
-        new MiniCssExtractPlugin({
-            filename: 'styles/[name].css',
-            chunkFilename: 'styles/[id].css',
-            //allChunks: true
-        }),
-
         // fix "process is not defined" error:
         new webpack.ProvidePlugin({
             process: 'process/browser',
         }),
+        new MiniCssExtractPlugin({
+            filename: 'styles/[name].css',
+            chunkFilename: 'styles/[id].css',
+        }),
+		new CopyWebpackPlugin({
+            patterns: [{
+                from: "static/",
+                to: "."
+            }],
+        }),
     ],
+    devServer: {
+        static: {
+            directory: './dist',
+        },
+    },
 
-	performance: {
-		hints: false,	// 不再提示性能问题
-		maxEntrypointSize: 512000,
-		maxAssetSize: 512000
-	},
+    performance: {
+        hints: false, // 不再提示性能问题
+        maxEntrypointSize: 512000,
+        maxAssetSize: 512000
+    },
+
     optimization: {
         minimize: false,
         minimizer: [
@@ -97,4 +86,4 @@ module.exports = {
             }),
         ],
     },
-};
+}
